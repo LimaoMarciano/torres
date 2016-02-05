f49ce1d5485bd056a57ffa067d055c509f0337e9
Marcelo <marceloapsilva@gmail.com>
2016-02-05 03:54:50 -0200

    Added separated construction for pieces and screws

---
+++ Torres/Assets/Version04/PieceCreator.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class PieceCreator : MonoBehaviour {
+
+	public GameObject piecePrefab;
+
+	private enum State {
+		setStartPoint,
+		adjustPiece,
+		setEndPoint
+	};
+		
+	private State state;
+	private Vector2 mousePosition;
+	private Vector2 startPoint;
+	private Vector2 endPoint;
+	private GameObject piece;
+	private EventSystem eventSystem;
+
+	// Use this for initialization
+	void Start () {
+		eventSystem = EventSystem.current;
+	}
+
+	void OnEnable () {
+		state = State.setStartPoint;
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		switch (state) {
+		case State.setStartPoint:
+
+			if (Input.GetMouseButtonDown (0)) {
+
+				//Check if is clicking on UI
+				if (eventSystem.IsPointerOverGameObject ()) {
+					return;
+				}
+
+				startPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				state = State.setEndPoint;
+				CreatePiece ();
+			}
+
+			break;
+
+		case State.setEndPoint:
+
+			if (!piece.GetComponent<SpriteRenderer> ().enabled) {
+				piece.GetComponent<SpriteRenderer> ().enabled = true;
+			}
+
+			mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			StrecthSprite (piece, startPoint, mousePosition);
+
+			if (Input.GetMouseButtonDown (0)) {
+
+				//Check if is clicking on UI
+				if (eventSystem.IsPointerOverGameObject ()) {
+					GameObject.Destroy (piece);
+					state = State.setStartPoint;
+					return;
+				}
+				endPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				state = State.setStartPoint;
+			}
+
+			break;
+		}
+	}
+
+	void CreatePiece() {
+		piece = Instantiate (piecePrefab, startPoint, Quaternion.identity) as GameObject;
+		piece.GetComponent<SpriteRenderer> ().enabled = false;
+		piece.GetComponent<Rigidbody2D> ().isKinematic = true;
+	}
+
+	private void StrecthSprite (GameObject sprite, Vector2 startPos, Vector2 endPos) {
+		Vector2 centerPos = (startPos + endPos) / 2f;
+		sprite.transform.position = centerPos;
+		Vector2 direction = startPos - endPos;
+		direction = direction.normalized;
+		sprite.transform.right = direction;
+
+		if (endPos.x >= startPos.x) {
+			sprite.transform.right *= -1f;
+		}
+		Vector2 scale = new Vector2(1,1);
+		scale.x = Vector2.Distance(startPos, endPos);
+		sprite.transform.localScale = new Vector3(scale.x, scale.y, 1);
+
+	}
+}
---
+++ Torres/Assets/Version04/ScrewCreator.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrewCreator : MonoBehaviour {
+	public GameObject screwPrefab;
+	private Vector2 mousePosition;
+	private GameObject pieceTarget;
+	private GameObject pieceConnected;
+
+	private bool isConstructionAllowed = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Collider2D[] colliders;
+
+		mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		colliders = Physics2D.OverlapPointAll(mousePosition, LayerMask.GetMask("Pieces"));
+
+		if (colliders.Length > 1) {
+			isConstructionAllowed = true;
+		} else {
+			isConstructionAllowed = false;
+		}
+
+		if (Input.GetMouseButtonDown (0) && isConstructionAllowed) {
+			pieceTarget = colliders [0].gameObject;
+			for (int i = 1; i < colliders.Length; i++) {
+				pieceConnected = colliders [i].gameObject;
+				CreateScrew (pieceTarget, pieceConnected, mousePosition);
+			}
+			GameObject sprite = Instantiate (screwPrefab, mousePosition, Quaternion.identity) as GameObject;
+			sprite.transform.SetParent (pieceTarget.transform);
+
+		}
+	}
+
+	private void CreateScrew (GameObject targetObj, GameObject connectedObj, Vector2 position) {
+		HingeJoint2D joint;
+		HingeJoint2D[] joints;
+		targetObj.AddComponent<HingeJoint2D> ();
+
+		joints = targetObj.GetComponents<HingeJoint2D> ();
+		joint = joints[joints.Length - 1];
+		joint.connectedBody = connectedObj.GetComponent<Rigidbody2D>();
+
+		Vector3 mousePositionVector3 = new Vector3 (mousePosition.x, mousePosition.y, 0);
+		joint.anchor = targetObj.transform.InverseTransformPoint (mousePositionVector3);
+		joint.connectedAnchor = connectedObj.transform.InverseTransformPoint (mousePositionVector3);
+
+		joint.breakForce = 500;
+	}
+}
